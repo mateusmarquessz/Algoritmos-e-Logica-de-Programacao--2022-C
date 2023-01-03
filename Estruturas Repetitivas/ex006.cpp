a14a588227b293d8edcecb5d6ef445744f165e10
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2023-01-03 19:24:19 -0300

    att

--- Estruturas Repetitivas/ex006.cpp
+++ Estruturas Repetitivas/ex006.cpp
@@ -3,3 +3,15 @@
 #include <cmath>
 #include <cstring>
 
+int main(){
+    float nota1,nota2, media;
+
+    printf("Digite a primeira nota");
+    scanf("%f", &nota1);
+    printf("Digite a segunda nota");
+    scanf("%f", &nota2);
+
+    while(nota1 != 0 && nota2 != 0){
+        media = nota1+ nota2 /2;
+    }
+}
