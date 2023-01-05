9e8c5a7136cc6f103a1d1285dc4d532c8fcc378f
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2023-01-05 11:04:24 -0300

    att

--- Estruturas Repetitivas/ex007.cpp
+++ Estruturas Repetitivas/ex007.cpp
@@ -4,8 +4,29 @@
 #include <cstring>
 
 int main(){
-    int alcool, gasolina, diesel, fim;
-    while(fim<4){
+    int alcool = 0, gasolina = 0, diesel = 0, fim;
+
+    printf("Informe um codigo (1, 2, 3) ou 4 para parar:"); /* Sempre comecar com a pergunta antes*/
+    scanf("%i", &fim);
+
+    while(fim != 4){
+      switch (fim){
+    case 1:
+        alcool = alcool + 1;
+    break;
+
+    case 2:
+        gasolina = gasolina + 1;
+    break;
+
+    case 3:
+        diesel = diesel + 1;
+    break;
+    default:
+    break;
+      }
         printf("Informe um codigo (1, 2, 3) ou 4 para parar:");
+        scanf("%i", &fim);
     }
+    printf("MUITO OBRIGADO \n Alcool: %i \n Gasolina: %i \n Diesel: %i", alcool, gasolina, diesel);
     }
