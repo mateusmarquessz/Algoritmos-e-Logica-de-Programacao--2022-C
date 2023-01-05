dca6416af7ba631d5eb54a9d78614523cda32bba
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2023-01-04 22:11:26 -0300

    att

--- Estruturas Repetitivas/ex006.cpp
+++ Estruturas Repetitivas/ex006.cpp
@@ -5,13 +5,21 @@
 
 int main(){
     float nota1,nota2, media;
-
     printf("Digite a primeira nota");
-    scanf("%f", &nota1);
-    printf("Digite a segunda nota");
-    scanf("%f", &nota2);
+        scanf("%f", &nota1);
+    while(nota1 < 0.10 || nota1 > 10){
+            printf("Valor invalido!Tente novamente:\n");
+            scanf("%f", &nota1);
 
-    while(nota1 != 0 && nota2 != 0){
-        media = nota1+ nota2 /2;
     }
+   printf("Digite a segunda nota");
+        scanf("%f", &nota2);
+
+    while(nota2 < 0.10 || nota2 > 10){
+
+            printf("Valor invalido!Tente novamente:\n");
+            scanf("%f", &nota2);
+    }
+    media = (nota1 + nota2) / 2;
+    printf("Media:%f", media);
 }
---
+++ Estruturas Repetitivas/ex007.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <cmath>
+#include <cstring>
+
+int main(){
+    int alcool, gasolina, diesel, fim;
+    while(fim<4){
+        printf("Informe um codigo (1, 2, 3) ou 4 para parar:");
+    }
+    }
