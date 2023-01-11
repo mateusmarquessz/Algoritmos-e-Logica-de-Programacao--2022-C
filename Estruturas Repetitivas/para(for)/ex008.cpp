af9a9ff03174613a255803d0401c5d096be52420
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2023-01-11 20:50:51 -0300

    att

--- Estruturas Repetitivas/para(for)/ex008.cpp
+++ Estruturas Repetitivas/para(for)/ex008.cpp
@@ -4,4 +4,16 @@
 #include <cstring>
 
 
-int main
+int main(){
+    int i, x;
+    double num1,num2,num3, media;
+    printf("Quantos casos voce vai digitar?");
+    scanf("%i", &x);
+
+    for(i=0; i<x; i++){
+        printf("Digite tres numeros:\n");
+        scanf("%f %f  %f", &num1, &num2 , &num3);
+        media = (num1 *2.0 + num2 * 3.0 + num3 * 5.0) / 10;
+        printf("\nMEDIA = %.1lf\n", media);
+    }
+}
