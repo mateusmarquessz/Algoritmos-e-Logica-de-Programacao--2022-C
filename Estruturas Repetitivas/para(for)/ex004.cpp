ed6c9317f526b20fbd98e7bfe72185b45f623b8d
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2023-01-11 11:27:49 -0300

    att

--- Estruturas Repetitivas/para(for)/ex004.cpp
+++ Estruturas Repetitivas/para(for)/ex004.cpp
@@ -4,10 +4,22 @@
 #include <cstring>
 
 int main(){
-    int i, x, y;
+    int i, x, y , troca, soma;
     printf("Digite o valor de X");
         scanf("%i", &x);
     printf("Digite o valor de Y");
         scanf("%i", &y);
-
+    if(x>y){
+        troca = x; /* trocando valores das variaveis*/
+        x = y;
+        y = troca;
+    }
+ soma = 0;
+    for(i = x+1; i<y-1; i++)/* Estrutura de repeticao para somar os impares*/
+    { 
+         if(i %2 !=0){
+            soma = soma + i;
+        }
+    }
+    printf("SOMA DOS IMPARES = %i", soma);
 }
