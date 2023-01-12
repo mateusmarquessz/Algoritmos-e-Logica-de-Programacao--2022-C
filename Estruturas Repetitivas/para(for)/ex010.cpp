3b9d7a6141385f1af398aa81bcbb725d81023f02
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2023-01-11 23:26:16 -0300

    att

--- Estruturas Repetitivas/para(for)/ex010.cpp
+++ Estruturas Repetitivas/para(for)/ex010.cpp
@@ -4,5 +4,11 @@
 #include <cstring>
 
 int main(){
-    
+    int i, n, fat;
+    printf("Digite um numero natural!");
+    scanf("%i", &n);
+    for(i=n; i>0; i--){
+        fat = fat * i;
+    }
+    printf("FATORIAL = %i", fat);
 }
