522960bfdc72b3eff81db559499a9b4a30d7a686
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2023-01-12 09:40:32 -0300

    att

---
+++ Estruturas Repetitivas/para(for)/ex011.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <cmath>
+#include <cstring>
+
+int main(){
+    int i, n, cobaias, totalratos = 0, totalcoelhos = 0, totalsapos = 0, totalCobaia;
+    char tipoCobaia;
+    float pRatos, pSapos, pCoelhos;
+    printf("Quantos casos de tese serao digitados?");
+    scanf("%i", &n);
+
+    for(i=0; i<n; i++){
+        printf("Quantidades de cobaias:");
+        scanf("%i", &cobaias);
+        printf("Tipo de cobaia:");
+        scanf("%s", &tipoCobaia);
+
+        if(tipoCobaia == 'r'){
+            totalratos = totalratos + cobaias;
+        } else if(tipoCobaia == 'c'){
+            totalcoelhos = totalcoelhos + cobaias;
+        } else if(tipoCobaia == 's'){
+            totalsapos = totalsapos + cobaias;
+        }
+    totalCobaia = totalcoelhos + totalratos + totalsapos;
+    pRatos = totalratos / totalCobaia * 100;
+    pSapos = totalsapos / totalCobaia * 100;
+    pCoelhos = totalcoelhos / totalCobaia * 100;
+    }
+    printf("\nRELATORIO FINAL\n");
+    printf("Total:%i \n", totalCobaia);
+    printf("Total de coelhos:%i \n",totalcoelhos);
+    printf("Total de ratos:%i \n", totalratos);
+    printf("Total de sapos:%i \n",totalsapos);
+    printf("Percentual de coelhos:%i \n", pCoelhos);
+    printf("Percentual de ratos:%i \n",pRatos);
+    printf("Percentual de sapos:%i \n",pSapos);
+}
