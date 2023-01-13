18acf21f33321e89e29c2b718fd95e93bed9fdf9
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2023-01-13 09:44:28 -0300

    att

--- Vetores/ex006.cpp
+++ Vetores/ex006.cpp
@@ -4,13 +4,23 @@
 #include <cstring>
 
 int main(){
-    int i, n, vet[10];
+    int i, n, vet[100], maior, posMaior;
     printf("Quantos numeros voce vai digitar?");
     scanf("%i", &n);
 
     for(i=0; i<n; i++){
         printf("Digite um numero:");
-        scanf("%i", vet[i]);
+        scanf("%i", &vet[i]);
     }
-    
+    maior = vet[0];
+    posMaior = 0;
+    for(i=0; i<n; i++){
+        if(vet[i] > maior){
+            maior = vet [i];
+            posMaior = i;
+        }
+    }
+    printf("MAIOR VALOR = %i", maior);
+    printf("\nPOSICAO DO MAIOR VALOR = %i", posMaior);
+
 }
